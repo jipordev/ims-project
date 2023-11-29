1663538e0ac5a8f90a7ec516a6dab67a01a9e093
SeangMengKheang <mxngkhxang026@gmail.com>
2023-11-29 13:53:02 +0700

    create database and dto

--- src/co/cstad/Main.java
+++ src/co/cstad/Main.java
@@ -1,10 +1,51 @@
 package co.cstad;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Bruh");
-        System.out.println("Hello");
-        System.out.println("World");
+import co.cstad.view.MenuView;
+import co.cstad.view.MenuViewAdmin;
+import co.cstad.view.MenuViewManager;
 
+import java.util.Scanner;
+
+public class Main {
+    static MenuView menuView = new MenuView();
+    static MenuViewAdmin menuViewAdmin = new MenuViewAdmin();
+    static MenuViewManager menuViewManager = new MenuViewManager();
+    public static void main(String[] args) {
+        menuView.startInterface();
+        try {
+            Thread.sleep(2000);
+            System.out.println("\n".repeat(20));
+            menuView.menuLogin();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        int option = Integer.parseInt(inputValue("Choose Option Between : 1 - 3: "));
+        switch (option) {
+            case 1 -> {
+                String username = inputValue("Enter Username: ");
+                String password = inputValue("Enter Password: ");
+
+                if (username.equals("admin") && password.equals("123")) {
+                    menuViewAdmin.mainMenu();
+                } else if (username.equals("manager") && password.equals("123")) {
+
+                    while (true) {
+                        menuViewManager.mainMenu();
+                        int optionManager = Integer.parseInt(inputValue("Choose Option Between : 1 - 4: "));
+                        switch (optionManager) {
+                            case 1 ->
+                        }
+                    }
+                } else {
+                    System.out.println("Invalid Username or Password");
+                }
+            }
+            case 2 -> menuView.menuAboutUs();
+            case 3 -> System.exit(0);
+        }
+    }
+    public static String inputValue(String message) {
+        System.out.print(message);
+        return new Scanner(System.in).nextLine();
     }
 }

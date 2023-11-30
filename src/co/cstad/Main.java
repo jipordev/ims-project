4650d9eda9c3c4f6a6d40f22e44852d935ebb72c
longpiseth2002 <stavkhat121212@gmail.com>
2023-11-30 11:44:14 +0700

    Ready prepare methods match className and prepare logic

--- src/co/cstad/Main.java
+++ src/co/cstad/Main.java
@@ -13,46 +13,142 @@ public class Main {
     static MenuViewManager menuViewManager = new MenuViewManager();
     static MenuViewReport menuViewReport = new MenuViewReport();
     public static void main(String[] args) {
-//        menuView.startInterface();
-//        try {
-//            Thread.sleep(2000);
-//            System.out.println("\n".repeat(20));
-//            menuView.menuLogin();
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-        int option = Integer.parseInt(inputValue("Choose Option Between : 1 - 3: "));
-        switch (option) {
-            case 1 -> {
-                String username = inputValue("Enter Username: ");
-                String password = inputValue("Enter Password: ");
-
-                if (username.equals("admin") && password.equals("123")) {
-                    menuViewAdmin.menuLogin();
-                } else if (username.equals("manager") && password.equals("123")) {
-
-                    while (true) {
-                        menuViewManager.mainMenu();
-                        int optionManager = Integer.parseInt(inputValue("Choose Option Between : 1 - 4: "));
-                        switch (optionManager) {
-                            //case 1 ->
-                        }
-                    }
-                } else if (username.equals("report") && password.equals("123")) {
-                    System.out.println("\n".repeat(20));
-                    menuViewReport.menuReport();
-                }
-                else {
-                    System.out.println("Invalid Username or Password");
-                }
-            }
-            case 2 -> {
-                System.out.println("\n".repeat(20));
-                menuView.menuAboutUs();
-
-            }
-            case 3 -> System.exit(0);
+        boolean check = false;
+        menuView.startInterface();
+        try {
+            Thread.sleep(2000);
+            System.out.println("\n".repeat(20));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
+
+
+        do{
+
+            menuView.menuLogin();
+            int option = Integer.parseInt(inputValue("  >> Choose Option Between [ 1 - 3 ] : "));
+
+            switch (option) {
+
+            // Case 1 : Log In
+                case 1 -> {
+
+                    System.out.println( " \n" + "_".repeat(50));
+                    String username = inputValue("  => Enter Username : ");
+                    String password = inputValue("  => Enter Password : ");
+                    System.out.println("_".repeat(50) + " \n");
+
+                // Admin logic
+                    if (username.equals("admin") && password.equals("123")) {
+                        menuViewAdmin.mainMenuAdmin();
+                        int optionAdmin;
+                        do{
+                            optionAdmin = Integer.parseInt(inputValue("  >> Choose Option Between [ 1 - 6 ] : "));
+                            switch (optionAdmin){
+                                case 1 ->{
+                                    menuView.itemMenu();
+                                }
+                                case 2 ->{
+                                    menuViewAdmin.customerMenu();
+                                }
+                                case 3 ->{
+                                    menuView.invoiceMenu();
+                                }
+                                case 4 ->{
+                                    menuViewAdmin.userMenu();
+                                }
+                                case 5 ->{
+                                    menuView.reportMenu();
+                                }
+                                case 6 ->{
+
+                                }
+                                default -> {
+                                    System.out.println("\n" + " ".repeat(5) + "INPUT IS INVALID !");
+                                    System.out.println(" ".repeat(5) + "PLEASE CHOOSE AN OPTION FROM 1 TO 6 .");
+                                }
+                            }
+
+                        }while(optionAdmin!=6);
+
+                    }
+
+
+                // Manager Logic
+                    else if (username.equals("manager") && password.equals("123")) {
+                        int optionManager;
+                        do{
+                            menuViewManager.mainMenuManager();
+                            optionManager = Integer.parseInt(inputValue("Choose Option Between [ 1 - 3 ] : "));
+                            switch (optionManager) {
+                                case 1 ->{
+                                    menuView.itemMenu();
+                                }
+                                case 2 ->{
+                                    menuView.invoiceMenu();
+                                }
+                                case 3 ->{
+                                    System.out.println("\n Back to Main Menu ");
+                                }
+                                default -> {
+                                    System.out.println("\n" + " ".repeat(5) + "INPUT IS INVALID !");
+                                    System.out.println(" ".repeat(5) + "PLEASE CHOOSE AN OPTION FROM 1 TO 3 .");
+                                }
+                            }
+                        }while(optionManager!=3);
+
+                    }
+
+
+
+                // Report Logic
+                    else if (username.equals("report") && password.equals("123")) {
+                        System.out.println("\n".repeat(20));
+                        menuViewReport.mainMenuReport();
+                        int optionReport;
+
+                        do{
+                            optionReport = Integer.parseInt(inputValue("Choose Option Between [ 1 - 2 ] : "));
+
+                            switch (optionReport){
+                                case 1 ->{
+                                    menuView.reportMenu();
+                                }
+                                case 2 -> {
+
+                                }
+                                default -> {
+                                    System.out.println("\n" + " ".repeat(5) + "INPUT IS INVALID !");
+                                    System.out.println(" ".repeat(5) + "PLEASE CHOOSE AN OPTION FROM 1 TO 2 .");
+                                }
+                            }
+                        }while (optionReport!=2);
+                    }
+                    else {
+                        System.out.println("Invalid Username or Password");
+                    }
+                }
+
+
+            // Case 2 : Display Information About Us
+                case 2 -> {
+                    System.out.println("\n".repeat(20));
+                    menuView.menuAboutUs();
+
+                }
+
+            // Case 3 : Exit Program
+                case 3 -> System.exit(0);
+
+
+
+
+                default -> {
+                    System.out.println("\n" + " ".repeat(5) + "INPUT IS INVALID !");
+                    System.out.println(" ".repeat(5) + "PLEASE CHOOSE AN OPTION FROM 1 TO 3 .");                }
+            }
+        }while(!check);
     }
     public static String inputValue(String message) {
         System.out.print(message);

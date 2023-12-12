c28be36b8ec481e01cc012500cb7b015ee7bd061
sokny2023 <psnpsn667788@gmail.com>
2023-12-12 15:53:21 +0700

    second report

--- src/co/cstad/controller/ReportController.java
+++ src/co/cstad/controller/ReportController.java
@@ -1,20 +1,20 @@
-package co.cstad.controller;
-
-import co.cstad.service.UserService;
-import co.cstad.util.Singleton;
-import co.cstad.view.MenuViewAdmin;
-
-import java.util.Scanner;
-
-public class ReportController {
-    private final MenuViewAdmin menuViewAdmin;
-    private final
-    private final Scanner scanner;
-
-    public ReportController(UserService userService){
-        scanner = Singleton.scanner();
-
-
-    }
-
-}
+//package co.cstad.controller;
+//
+//import co.cstad.service.UserService;
+//import co.cstad.util.Singleton;
+//import co.cstad.view.MenuViewAdmin;
+//
+//import java.util.Scanner;
+//
+//public class ReportController {
+//    private final MenuViewAdmin menuViewAdmin;
+//    private final
+//    private final Scanner scanner;
+//
+//    public ReportController(UserService userService){
+//        scanner = Singleton.scanner();
+//
+//
+//    }
+//
+//}
